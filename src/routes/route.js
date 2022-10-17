3512543051ce50985a40d22898879bde193e11b9
rjROhIT1312 <joshirohit44@gmail.com>
2022-10-17 19:36:13 +0530

    all done

--- src/routes/route.js
+++ src/routes/route.js
@@ -14,7 +14,83 @@ router.get('/students', function (req, res){
     res.send(students)
 })
 
+//q1)
+router.get('/movies', function (req, res){
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    res.send(movies)
+})
 
+//q4
+router.get('/films', function (req, res){
+    let film = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       
+    res.send(film)
+})
+
+//q2 & q3
+
+router.get('/movies/:index', function (req,res){
+    let inParams = req.params
+    let movies=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    if(inParams.index>movies.length){
+       res.send("not valid index")
+    }
+   
+   
+   res.send(movies[inParams.index])
+})
+
+//q5
+
+router.get('/films/:id', function (req,res){
+
+    let idParam=req.params
+    
+    const film=[ {
+        id: 1,
+        name: "The Shining"
+       },
+        {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+
+       
+       if(idParam.id>(film.length+1)||idParam.id==0){
+        res.send("not valid")
+        
+    }      
+       
+       
+       for(i of film){
+
+       
+        
+        
+        if(i.id==idParam.id){
+            console.log(i)
+            res.send(i.name)
+        }
+       }
+})
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
     // ':' denotes that the following part of route is a variable
