e72616db371902507335fd985bda8d92d7c95b4a
rjROhIT1312 <joshirohit44@gmail.com>
2022-10-26 12:18:52 +0530

    done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -32,12 +32,18 @@ const  bookSchema = new mongoose.Schema( {
         unique: true,
         required: true
     },
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    year: {type: Number, default: 2021},
+    tags: [String],
     authorName: {
         type: String,
         required: true
     },
-    category: String,
-    year: String,
+    totalPages: Number,
+    stockAvailable: Boolean
 }, { timestamps: true });
 
 module.exports = mongoose.model('Books', bookSchema)
