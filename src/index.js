44a4cafa97945f197404473ffaf61589a72915f3
rjROhIT1312 <joshirohit44@gmail.com>
2022-11-03 11:10:40 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rjrohit13:Rohit817@cluster0.0zq8uvw.mongodb.net/rohit-db", {
     useNewUrlParser: true
 })
 
