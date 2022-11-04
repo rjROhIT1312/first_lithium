06fbccd97e623b3e5685cf60edf387e406537887
rjROhIT1312 <joshirohit44@gmail.com>
2022-11-04 14:24:28 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rjrohit13:Rohit817@cluster0.0zq8uvw.mongodb.net/rohit-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema(
+  {
     firstName: String,
     lastName: String,
     mobile: {
-        type: String,
-
-        required: true
+    type: String,
+    required: true,
     },
     emailId: String,
     password: String,
     gender: {
-        type: String,
-        enum: ["male", "female", "other"]
+    type: String,
+    enum: ["male", "female", "other"],
     },
     age: Number,
-}, { timestamps: true });
+    isDeleated: {
+    type: Boolean,
+    default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
