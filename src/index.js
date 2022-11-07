58d348ce6239571b83e25277e5eeffa274458d91
rjROhIT1312 <joshirohit44@gmail.com>
2022-11-07 13:21:22 +0530

    oookk

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rjrohit13:Rohit817@cluster0.0zq8uvw.mongodb.net/rohit-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
 
 app.use('/', route)
 
