f4d8ac7af1b30cc06a44908c06fb295f74ffcdbf
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-14 13:38:12 +0530

    Author , blog model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require("./routes/route")
+const mongoose  = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://rjrohit13:bThZ1H4sEACa5DqQ@cluster0.fo02cni.mongodb.net/Project1", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
