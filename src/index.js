aab784261bbfeb9a9487c467c04c24102a182a4b
kunalsingh0709 <uic.19bca1123@gmail.com>
2022-11-21 12:49:17 +0530

    1st_edit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-Parser")
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app = express()
+
+app.use(bodyParser.json());   
+
+mongoose.connect("mongodb+srv://kunal0709:Singhkunal7@cluster0.u5yk4f2.mongodb.net/project2",{
+    useNewUrlParser:true  
+}) //promise return
+
+.then(()=> console.log("MongoDB is connected"))  
+.catch(err => console.log(err))
+
+
+app.use("/",route) 
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("express app runing on port "+(process.env.PORT || 3000) )
+})
+
+
